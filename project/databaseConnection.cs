26b8e6dc0fe24b6ed541bf2c9e9b7c3d28e9683e
ArifurRahmanShezan <arifurrahmanshezan@gmail.com>
2024-10-05 19:24:05 +0600

    First Commit

---
+++ project/databaseConnection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    public class databaseConnection
+    {
+        public static string database()
+        {
+
+            string s = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\personal\\SQLDATABASENEW.mdf;Integrated Security=True;Connect Timeout=30";
+
+            return s;
+
+        }
+    }
+}
+
